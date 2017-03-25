96742b79b8a8d7bc1bad11e9269afcab84537558
Damien Gavard <damien.gavard@gmail.com>
2017-03-25 12:56:45 +0100

    Add test for specific number of success

--- MaxibonKataIOSTests/KarumiHQsSpecs.swift
+++ MaxibonKataIOSTests/KarumiHQsSpecs.swift
@@ -14,6 +14,16 @@ class KarumiHQsSpecs: XCTestCase {
             return headQuarter.maxibonsLeft > 2
         }
 
+        property("There are always at least 2 maxibons left after ONE developer open the fridge and to it untill we have 10 success",
+                 arguments: CheckerArguments(maxAllowableSuccessfulTests: 10)) <- forAll { (developer: Developer) in
+            let headQuarter = KarumiHQs()
+            headQuarter.openFridge(developer)
+
+            print("There are \(headQuarter.maxibonsLeft) maxibons left after \(developer.name) opened the fridge")
+
+            return headQuarter.maxibonsLeft > 2
+        }
+
         property("There are always at least 2 maxibons left after MANY developers open the fridge") <- forAll { (developers: [Developer]) in
             let headQuarter = KarumiHQs()
             headQuarter.openFridge(developers)
